d3f28268b69e44aabe6d31ef70b3c2199d3da599
Max test <botass740@gmail.com>
2023-06-06 17:39:19 +0700

    Update HW

---
+++ src/main/java/ru/netology/javaqamvn/Radio.java
@@ -0,0 +1,63 @@
+package ru.netology.javaqamvn;
+
+public class Radio {
+
+    private int currentStation;
+    private int currentVolume;
+
+    public void next() {
+        if (currentStation != 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void up() {
+        if (currentVolume != 100) {
+            currentVolume++;
+        }
+    }
+
+    public void down() {
+        if (currentVolume != 0) {
+            currentVolume --;
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        this.currentVolume = newCurrentVolume;
+    }
+}
